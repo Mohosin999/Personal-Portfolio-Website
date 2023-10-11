a001d0449ac4efae16164b562e4991e48a0ccdf3
Mohosin999 <mohosin.h.akash@gmail.com>
2023-10-11 08:40:56 +0600

    initial commit

--- app/page.js
+++ app/page.js
@@ -3,14 +3,18 @@ import Wrapper from "./components/wrapper";
 import Image from "next/image";
 import profilePic from "../public/images/akash.jpg";
 import AnimatedText from "./components/animated-text";
+import Link from "next/link";
+import { LinkArrow } from "./components/icons";
 
 const Home = () => {
   return (
     <Wrapper className="pt-0">
       <div class="flex items-center justify-between w-full">
+        {/* Right side image */}
         <div class="w-1/2">
           <Image src={profilePic} alt="profile-photo" class="w-3/4 h-auto" />
         </div>
+        {/* Left side title, text and buttons - start */}
         <div class="w-1/2 flex flex-col items-center self-center">
           <AnimatedText
             text={"A passionate frontend web developer."}
@@ -23,7 +27,24 @@ const Home = () => {
             user interface (UI), ensuring that web pages are visually appealing,
             responsive, and user-friendly.
           </p>
+          <div class="flex items-center self-start mt-2">
+            <Link
+              href="/dummy.pdf"
+              target="_blank"
+              class="flex items-center bg-gray-900 text-gray-200 p-2.5 px-6 rounded-lg text-lg font-semibold hover:bg-gray-200 hover:text-gray-900 border-2 border-solid border-transparent hover:border-gray-900"
+            >
+              Resume <LinkArrow class="w-6 ml-1" />
+            </Link>
+            <Link
+              href="mailto:mohosin.h.akash@gmail.com"
+              target="_blank"
+              class="ml-4 text-lg font-medium capitalize text-gray-900 underline"
+            >
+              Contact
+            </Link>
+          </div>
         </div>
+        {/* Left side title, text and buttons - end */}
       </div>
     </Wrapper>
   );
