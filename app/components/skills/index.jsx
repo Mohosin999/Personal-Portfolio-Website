aeaa76fa6015c6a2dc5436b9196d516a158c17aa
Mohosin999 <mohosin.h.akash@gmail.com>
2023-10-12 07:44:44 +0600

    working with skills section to add slide in skills

--- app/components/skills/index.jsx
+++ app/components/skills/index.jsx
@@ -1,15 +1,40 @@
+"use client";
 import React from "react";
-import AnimatedText from "../animated-text";
+import { motion } from "framer-motion";
+
+// This reusable component for slide all skills
+const SkillSlider = ({ skillsProp, initialX }) => {
+  return (
+    <ul>
+      {skillsProp.map((item, index) => (
+        <motion.li
+          key={index}
+          initial={{ x: initialX }}
+          whileInView={{ x: 0 }}
+          transition={{ duration: 2, type: "spring" }}
+          className="text-2xl"
+        >
+          {item}
+        </motion.li>
+      ))}
+    </ul>
+  );
+};
 
 const Skills = () => {
+  const leftSkills = ["JavaScript", "React.js", "Next.js", "Redux"];
+  const rightSkills = ["Node.js", "Express.js", "Strapi", "Git & GitHub"];
   return (
     <>
       <h2 className="font-bold text-8xl mt-28 w-full text-center">Skills</h2>
       {/* <div className="w-full h-screen relative flex items-center justify-center rounded-full"></div> */}
-      <div className="flex flex-col items-center justify-center">
-        <AnimatedText text="JavaScript" className="text-4xl" />
-        <AnimatedText text="React JS" className="text-4xl" />
-        <AnimatedText text="Next JS" className="text-4xl" />
+      <div className="w-[50%] mx-auto mt-16 flex items-center justify-between">
+        <div>
+          <SkillSlider skillsProp={leftSkills} initialX={-400} />
+        </div>
+        <div>
+          <SkillSlider skillsProp={rightSkills} initialX={400} />
+        </div>
       </div>
     </>
   );
