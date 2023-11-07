0e819e2cd73d60de3b9cda985c1df9d20e3fa872
Mohosin999 <mohosin.h.akash@gmail.com>
2023-11-07 09:54:42 +0600

    install package 'next'

--- app/components/navbar/index.jsx
+++ app/components/navbar/index.jsx
@@ -113,71 +113,77 @@ const Navbar = () => {
           </nav>
         </div>
 
-        {/* Mobile menu */}
-        <div className="min-w-[70vw] flex flex-col z-30 items-center justify-between fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-gray-900/90 dark:bg-gray-100/75 rounded-lg backdrop-blur-md py-32">
-          {/* Left section of the navbar. */}
-          <nav>
-            <CustomLink href="/" title="Home" className="mr-4" />
-            <CustomLink href="/about" title="About" className="mx-4" />
-            <CustomLink href="/projects" title="Projects" className="mx-4" />
-            <CustomLink href="/articles" title="Articles" className="ml-4" />
-          </nav>
+        {/*
+         * ==========================================================================
+         * Mobile menu
+         * ==========================================================================
+         */}
+        {isOpen && (
+          <div className="min-w-[70vw] flex flex-col z-30 items-center justify-between fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-gray-900/90 dark:bg-gray-100/75 rounded-lg backdrop-blur-md py-32">
+            {/* Left section of the navbar. */}
+            <nav>
+              <CustomLink href="/" title="Home" className="mr-4" />
+              <CustomLink href="/about" title="About" className="mx-4" />
+              <CustomLink href="/projects" title="Projects" className="mx-4" />
+              <CustomLink href="/articles" title="Articles" className="ml-4" />
+            </nav>
 
-          {/**
-           * Right section of the navbar.
-           * Social links
-           */}
-          <nav className="flex flex-wrap items-center justify-center">
-            <motion.a
-              href="https://web.facebook.com/MohosinH99"
-              target="_blank"
-              whileHover={{ y: -2 }}
-              whileTap={{ scale: 0.9 }}
-              className="w-6 mr-3"
-            >
-              <FacebookIcon />
-            </motion.a>
-            <motion.a
-              href="https://github.com/Mohosin999"
-              target="_blank"
-              whileHover={{ y: -2 }}
-              whileTap={{ scale: 0.9 }}
-              className="w-6 mx-3"
-            >
-              <GithubIcon />
-            </motion.a>
-            <motion.a
-              href=""
-              target="_blank"
-              whileHover={{ y: -2 }}
-              whileTap={{ scale: 0.9 }}
-              className="w-6 mx-3"
-            >
-              <TwitterIcon />
-            </motion.a>
-            <motion.a
-              href=""
-              target="_blank"
-              whileHover={{ y: -2 }}
-              whileTap={{ scale: 0.9 }}
-              className="w-6 ml-3"
-            >
-              <LinkedInIcon />
-            </motion.a>
+            {/**
+             * Right section of the navbar.
+             * Social links
+             */}
+            <nav className="flex flex-wrap items-center justify-center">
+              <motion.a
+                href="https://web.facebook.com/MohosinH99"
+                target="_blank"
+                whileHover={{ y: -2 }}
+                whileTap={{ scale: 0.9 }}
+                className="w-6 mr-3"
+              >
+                <FacebookIcon />
+              </motion.a>
+              <motion.a
+                href="https://github.com/Mohosin999"
+                target="_blank"
+                whileHover={{ y: -2 }}
+                whileTap={{ scale: 0.9 }}
+                className="w-6 mx-3"
+              >
+                <GithubIcon />
+              </motion.a>
+              <motion.a
+                href=""
+                target="_blank"
+                whileHover={{ y: -2 }}
+                whileTap={{ scale: 0.9 }}
+                className="w-6 mx-3"
+              >
+                <TwitterIcon />
+              </motion.a>
+              <motion.a
+                href=""
+                target="_blank"
+                whileHover={{ y: -2 }}
+                whileTap={{ scale: 0.9 }}
+                className="w-6 ml-3"
+              >
+                <LinkedInIcon />
+              </motion.a>
 
-            {/* Theme switcher button */}
-            <button
-              onClick={() => setMode(mode === "light" ? "dark" : "light")}
-              className="ml-3 flex items-center justify-center rounded-full p-1"
-            >
-              {mode === "dark" ? (
-                <SunIcon className="fill-gray-900" />
-              ) : (
-                <MoonIcon className="fill-gray-900" />
-              )}
-            </button>
-          </nav>
-        </div>
+              {/* Theme switcher button */}
+              <button
+                onClick={() => setMode(mode === "light" ? "dark" : "light")}
+                className="ml-3 flex items-center justify-center rounded-full p-1"
+              >
+                {mode === "dark" ? (
+                  <SunIcon className="fill-gray-900" />
+                ) : (
+                  <MoonIcon className="fill-gray-900" />
+                )}
+              </button>
+            </nav>
+          </div>
+        )}
       </div>
     </Wrapper>
   );
