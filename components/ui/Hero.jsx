6bc7337c7a962e2c4e646ace8149ad685d689fcd
Mohosin999 <mohosin.hasan.akash@gmail.com>
2024-12-30 22:17:55 +0600

    Hero area almost completed

--- components/ui/Hero.jsx
+++ components/ui/Hero.jsx
@@ -1,9 +1,10 @@
 import { FaDownload } from "react-icons/fa6";
+import { FaGithub, FaTwitter, FaLinkedin, FaFacebook } from "react-icons/fa";
 
 const Hero = () => {
   return (
-    <div className="w-full h-full flex items-center justify-center">
-      <div className="w-11/12 h-full flex items-center justify-center">
+    <div className="w-full h-full flex flex-col items-center justify-center">
+      <div className="w-11/12 h-full pt-24 flex items-center justify-center">
         {/* Right side hero section */}
         <div className="flex-1">
           <div className="text_container">
@@ -17,7 +18,10 @@ const Hero = () => {
               Hi, I'm Mohosin Hasan Akash, a frontend web developer from
               Bangladesh. I specialize in building web applications using React
               and Express. My passion is crafting user-friendly and visually
-              appealing digital experiences.
+              appealing digital experiences. I'm Mohosin Hasan Akash, a frontend
+              web developer from Bangladesh. I specialize in building web
+              applications using React and Express. My passion is crafting
+              user-friendly and visually appealing digital experiences.
             </p>
             {/* Download resume button */}
             <a href="./resume.pdf" target="_blank">
@@ -29,7 +33,80 @@ const Hero = () => {
         </div>
 
         {/* Left side hero section */}
-        <div className="flex-1"></div>
+        <div className="flex-1 flex items-center justify-center">
+          <div className="hero_img_container w-full max-w-md bg-secondarycolor p-3 rounded-3xl border border-gray-600/[0.5] shadow-md">
+            <img
+              src="./img/hero.jpg"
+              alt="profile_image"
+              className="w-full h-full rounded-3xl"
+            />
+          </div>
+        </div>
+      </div>
+
+      {/* ========================================================
+       *                      Social links
+       ======================================================== */}
+      <div className="w-11/12 h-fit pt-10 pb-4 flex items-center justify-center gap-5">
+        {/* Left section */}
+        <div className="w-7/12 py-6 flex items-center justify-around bg-secondarycolor p-3 rounded-3xl border border-gray-600/[0.5] shadow-md">
+          {/* Time of experience */}
+          <div className="flex items-center justify-center gap-3 border-r-2 border-gray-400 w-full">
+            <div className="font-bold text-5xl text-primarycolor">4+</div>
+            <div>
+              <h4 className="text-sm">
+                Years of <br /> Experience
+              </h4>
+            </div>
+          </div>
+          {/* Number of completed projects */}
+          <div className="flex items-center justify-center w-full gap-3">
+            <div className="font-bold text-5xl text-primarycolor">100+</div>
+            <div>
+              <h4 className="text-sm">
+                Projects <br /> Completed
+              </h4>
+            </div>
+          </div>
+        </div>
+
+        {/* Right section */}
+        <div className="w-5/12 py-5 flex items-center justify-around bg-secondarycolor p-3 rounded-3xl border border-gray-600/[0.5] shadow-md">
+          <ul className="flex items-center justify-center gap-6">
+            {/* GitHub */}
+            <li>
+              <a href="#">
+                <button className="w-14 h-14 rounded-full flex justify-center items-center bg-gray-700 border border-gray-600/[0.5] transition-all duration-200 ease-in-out hover:bg-primarycolor">
+                  <FaGithub className="w-8 h-8 text-blue-300" />
+                </button>
+              </a>
+            </li>
+            {/* Twitter */}
+            <li>
+              <a href="#">
+                <button className="w-14 h-14 rounded-full flex justify-center items-center bg-gray-700 border border-gray-600/[0.5] transition-all duration-200 ease-in-out hover:bg-primarycolor">
+                  <FaTwitter className="w-8 h-8 text-blue-300" />
+                </button>
+              </a>
+            </li>
+            {/* Linkedin */}
+            <li>
+              <a href="#">
+                <button className="w-14 h-14 rounded-full flex justify-center items-center bg-gray-700 border border-gray-600/[0.5] transition-all duration-200 ease-in-out hover:bg-primarycolor">
+                  <FaLinkedin className="w-8 h-8 text-blue-300" />
+                </button>
+              </a>
+            </li>
+            {/* Facebook */}
+            <li>
+              <a href="#">
+                <button className="w-14 h-14 rounded-full flex justify-center items-center bg-gray-700 border border-gray-600/[0.5] transition-all duration-200 ease-in-out hover:bg-primarycolor">
+                  <FaFacebook className="w-8 h-8 text-blue-300" />
+                </button>
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
