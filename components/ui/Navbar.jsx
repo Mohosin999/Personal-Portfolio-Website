fe3176b73f25a382454250f4e32e7f3dc9e59dae
Mohosin999 <mohosin.hasan.akash@gmail.com>
2025-01-02 14:45:50 +0600

    basic design completed

--- components/ui/Navbar.jsx
+++ components/ui/Navbar.jsx
@@ -42,11 +42,11 @@ const Navbar = () => {
       </ul>
 
       {/* Hire me button */}
-      <a href="#">
+      <Link href="/contact">
         <button className="w-full text-sm bg-primarycolor px-6 py-2 rounded-2xl hover:opacity-85 transition-all ease-out duration-200 whitespace-nowrap ">
           Hire Me
         </button>
-      </a>
+      </Link>
     </div>
   );
 };
